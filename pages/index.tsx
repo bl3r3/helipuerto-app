3bf24a5aeeb43b0038f2bdbec53f141359c60e9d
Roberto Carrasquel <rcarrasquel@makingsense.com>
2023-08-09 23:40:55 -0400

    feat: add video and animatios home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,64 @@
+"use client";
+import { useEffect } from "react";
 import Footer from "../components/ui/Footer";
 import Navbar from "../components/ui/Navbar";
 import Image from "next/image";
 import Placeholder from "../public/placeholder.svg";
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function About() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <>
       <Navbar />
-      <section className="w-full px-4 img-bg-main h-screen">
-        <div
+      <section className="w-full mx-auto h-screen relative">
+        {/* <div
           className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
-        >
-          <div className="container mx-auto px-4 h-full md:w-full">
-            <div className="flex content-center items-center justify-center h-full flex-col">
-              <h1 className="text-4xl">Titulo Principal</h1>
-              <h2 className="bg-white text-black text-xl w-full text-center">
-                SUBTITULO
-              </h2>
-              <div className="w-full flex flex-col md:flex-row md:items-center md:justify-center gap-2">
-                <button className="bg-lime-400 text-black text-xl p-2 text-center mt-4 rounded-md">
-                  Boton Uno
-                </button>
-                <button className="bg-transparent text-white text-xl p-2 text-center mt-4 rounded-md border-white border-2">
-                  Boton Dos
-                </button>
-              </div>
+        > */}
+        <video className="w-full p-0" autoPlay muted loop id="myVideo">
+          <source src="./video.mp4" type="video/mp4" />
+        </video>
+        <div className="container px-4 mx-auto h-full md:w-full absolute top-0 left-0 right-0">
+          <div className="flex content-center items-center justify-center h-full flex-col top-50">
+            <h1 className="text-4xl animate__animated animate__backInUp">
+              Titulo Principal
+            </h1>
+            <h2 className="bg-white text-black text-xl w-full text-center animate__animated animate__backInUp animate__delay-1s">
+              SUBTITULO
+            </h2>
+            <div className="w-full flex flex-col md:flex-row md:items-center md:justify-center gap-2 animate__delay-2s animate__animated animate__fadeIn">
+              <button className="bg-lime-400 text-black text-xl p-2 text-center mt-4 rounded-md">
+                Boton Uno
+              </button>
+              <button className="bg-transparent text-white text-xl p-2 text-center mt-4 rounded-md border-white border-2">
+                Boton Dos
+              </button>
             </div>
           </div>
         </div>
+        {/* </div> */}
       </section>
       <section className="w-full px-4 py-8 flex flex-col gap-4 md:flex-row md:px-12 lg:px-24 text-content">
-        <div className="w-full flex flex-col justify-center items-end bg-first-card border-2 rounded-md h-56 p-4 md:w-6/12 md:h-[270px]">
+        <div
+          className="w-full flex flex-col justify-center items-end bg-first-card border-2 rounded-md h-56 p-4 md:w-6/12 md:h-[270px]"
+          data-aos="slide-right"
+        >
           <h3 className="text-xl text-title">Horario</h3>
           <p>Lun - Vie 08:00 am - 11:00 pm</p>
           <p>Lun - Vie 08:00 am - 11:00 pm</p>
           <p>Lun - Vie 08:00 am - 11:00 pm</p>
         </div>
-        <div className="w-full flex flex-col justify-center items-start bg-second-card border-2 rounded-md h-56 p-4 md:w-6/12  md:h-[270px]">
+        <div
+          className="w-full flex flex-col justify-center items-start bg-second-card border-2 rounded-md h-56 p-4 md:w-6/12  md:h-[270px]"
+          data-aos="slide-left"
+        >
           <h3 className="text-xl text-title">Horario</h3>
           <p>Lun - Vie 08:00 am - 11:00 pm</p>
           <p>Lun - Vie 08:00 am - 11:00 pm</p>
@@ -50,7 +71,10 @@ export default function About() {
         </h3>
         <p className="text-xl text-content">Ven y diviertete con nosotros</p>
         <div className="flex flex-col md:flex-row md:flex-wrap">
-          <div className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12 ">
+          <div
+            className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12   "
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Que es el Helipuerto?</h4>
             <p className="text-sm text-center text-content">
@@ -60,7 +84,10 @@ export default function About() {
               temporibus voluptatibus consectetur.
             </p>
           </div>
-          <div className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12">
+          <div
+            className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Juega con nosotros</h4>
             <p className="text-sm text-center text-content">
@@ -70,7 +97,10 @@ export default function About() {
               temporibus voluptatibus consectetur.
             </p>
           </div>
-          <div className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12">
+          <div
+            className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Como Agendar un Turno</h4>
             <p className="text-sm text-center text-content">
@@ -80,7 +110,10 @@ export default function About() {
               temporibus voluptatibus consectetur.
             </p>
           </div>
-          <div className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12">
+          <div
+            className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Quieres ser VIP?</h4>
             <p className="text-sm text-center text-content">
@@ -90,7 +123,10 @@ export default function About() {
               temporibus voluptatibus consectetur.
             </p>
           </div>
-          <div className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12">
+          <div
+            className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Fotos y Videos</h4>
             <p className="text-sm text-center text-content">
@@ -100,7 +136,10 @@ export default function About() {
               temporibus voluptatibus consectetur.
             </p>
           </div>
-          <div className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12">
+          <div
+            className="w-full flex flex-col p-4 gap-2 items-center justify-center md:w-4/12"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Nuestros servicios</h4>
             <p className="text-sm text-center text-content">
@@ -118,7 +157,10 @@ export default function About() {
           Escoge el que mejor se adapte a ti
         </p>
         <div className="flex flex-col md:flex-row md:flex-wrap">
-          <div className="w-full flex flex-col gap-2 items-center justify-center md:w-4/12 p-4">
+          <div
+            className="w-full flex flex-col gap-2 items-center justify-center md:w-4/12 p-4"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Servicio Uno</h4>
             <p className="text-sm text-center text-content">
@@ -130,7 +172,10 @@ export default function About() {
             </button>
           </div>
 
-          <div className="w-full flex flex-col gap-2 items-center justify-center md:w-4/12 p-4">
+          <div
+            className="w-full flex flex-col gap-2 items-center justify-center md:w-4/12 p-4"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Servicio Uno</h4>
             <p className="text-sm text-center text-content">
@@ -142,7 +187,10 @@ export default function About() {
             </button>
           </div>
 
-          <div className="w-full flex flex-col gap-2 items-center justify-center md:w-4/12 p-4">
+          <div
+            className="w-full flex flex-col gap-2 items-center justify-center md:w-4/12 p-4"
+            data-aos="fade-in"
+          >
             <Image src={Placeholder} width={200} height={200} alt="heli" />
             <h4 className="text-lg text-title">Servicio Uno</h4>
             <p className="text-sm text-center text-content">
@@ -158,11 +206,19 @@ export default function About() {
       <section className="w-full px-4 img-bg-second h-[600px]">
         <div className="container mx-auto px-4 h-full md:w-full">
           <div className="flex content-center items-center justify-center h-full flex-col">
-            <h1 className="text-4xl">Titulo Principal</h1>
-            <h2 className="bg-white text-black text-xl w-full text-center">
+            <h1 className="text-4xl" data-aos="slide-left">
+              Titulo Principal
+            </h1>
+            <h2
+              className="bg-white text-black text-xl w-full text-center"
+              data-aos="slide-left"
+            >
               SUBTITULO
             </h2>
-            <div className="w-full flex flex-col md:flex-row md:items-center md:justify-center gap-2">
+            <div
+              className="w-full flex flex-col md:flex-row md:items-center md:justify-center gap-2"
+              data-aos="slide-left"
+            >
               <button className="bg-lime-400 text-black text-xl p-2 text-center mt-4 rounded-md">
                 Boton Uno
               </button>
