d900f07170bfd15b8238e71035421bc5325702d2
Roberto Carrasquel <rcarrasquel@makingsense.com>
2023-08-25 15:55:23 -0400

    changes

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LogoFooter from "../../public/logo.png";
+import LogoFooter from "../../public/img/logo-cut.png";
 import Image from "next/image";
 import Link from "next/link";
 
